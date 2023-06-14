6932d41c70ab5df5a10c242d6407243b21de99a6
andresg19 <andygermain17@gmail.com>
2023-06-14 18:22:49 -0300

    heyandes corregido en nav

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = ({handle}) => {
         <nav className='bg-[#0f0e0ee0] min-h-[8vh] lg:min-h-[15vh]'>
             <ul className='flex list-none'>
                 <li className='ml-10 text-[#eaff057b] lg:text-[20px]'>
-                    <h1>HeyAndres</h1>
+                    <h1>HeyAndes</h1>
                 </li>
                 <li className= 'ml-3 mt-5 lg:mx-auto'>
                     <select className='bg-transparent cursor-grab text-slate-300 text-[12px] outline-none lg:text-[14px]' name="" id="" onChange={handle}>
